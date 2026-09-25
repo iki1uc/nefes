c92022829ada3edd00abe519c71103a2c29cf51c
iki1uc <entertrainer@web.de>
2026-09-25 10:15:34 +0200

    Update justice.js

--- justice.js
+++ justice.js
@@ -1,4 +1,16 @@
-blau   = 2   schach
-grün   = 3   soccer
-gelb   = 4   wetter
-violett = 5  eigenes
+const JUSTICE = (() => {
+  const tafel = [
+    { farbe: "blau",    zahl: 2, name: "schach"  },
+    { farbe: "grün",    zahl: 3, name: "soccer"  },
+    { farbe: "gelb",    zahl: 4, name: "wetter"  },
+    { farbe: "violett", zahl: 5, name: "eigenes" },
+  ];
+
+  function farbe(zahl)  { return tafel.find(t => t.zahl === zahl); }
+  function zahl(name)   { return tafel.find(t => t.name === name); }
+  function alle()       { return [...tafel]; }
+
+  return { farbe, zahl, alle, tafel };
+})();
+
+if (typeof window !== "undefined") window.JUSTICE = JUSTICE;
